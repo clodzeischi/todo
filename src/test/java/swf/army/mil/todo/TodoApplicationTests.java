7c105dc7bf224595beb30cf2b5dbe3e51e436866
Constantin Lodzeischi <constantin.lodzeischi.mil@army.mil>
2025-09-26 09:40:02 -0500

    initial

---
+++ src/test/java/swf/army/mil/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package swf.army.mil.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
